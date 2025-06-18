e3ce0a2ec4b4a38f9d89683afe9514e70a348ff0
dfacoet <11651258+dfacoet@users.noreply.github.com>
2025-06-18 17:06:06 +0100

    Joins

---
+++ scripts/joins.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+import ibis
+import pyarrow
+
+JOIN_KEYS = ["time", "size"]
+
+time_index = [datetime(1989, 1, d) for d in range(1, 5)]
+
+left_table = pyarrow.Table.from_pydict(
+    {
+        "time": [t for t in time_index for _ in range(2)],
+        "size": ["S", "M"] * len(time_index),
+        "A": [chr(60 + x) for x in range(len(time_index))] * 2,
+        "value": [2 * x for x in range(2 * len(time_index))],
+    }
+)
+
+
+right_table = pyarrow.Table.from_pydict(
+    {
+        "time": [t for t in time_index for _ in range(2)],
+        "size": ["M", "L"] * len(time_index),
+        "B": [chr(x + 100) for x in range(len(time_index))] * 2,
+        "value": [1 + 2 * x for x in range(2 * len(time_index))],
+    }
+)
+
+print("left:")
+print(left_table.to_pandas())
+print("right:")
+print(right_table.to_pandas())
+
+con = ibis.duckdb.connect(":memory:")
+left = con.create_table("left_table", left_table)
+right = con.create_table("right_table", right_table)
+
+for how in ["inner", "left", "right", "outer"]:
+    query = ibis.join(left, right, how=how, predicates=JOIN_KEYS)
+    print(f"Performing a join with how={how}")
+
+    # print("Query:")
+    # print(ibis.to_sql(query))
+    result = query.to_pandas()
+    print(result)
+
+    if how == "inner":
+        continue
+    # Coalesce and select
+    print("Select (coalesced) join keys, and values")
+    query = query.select(
+        *(ibis.coalesce(ibis._[c], ibis._[c + "_right"]).name(c) for c in JOIN_KEYS),
+        "value",
+        "value_right",
+        (ibis._["value"] / ibis._["value_right"]).name("ratio"),
+        (ibis._["value"].fill_null(0) / ibis._["value_right"].fill_null(0)).name(
+            "ratio_reg"
+        ),
+    )
+
+    print(query.to_pandas())
