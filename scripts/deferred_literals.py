9a01ea60baa3342293920ec2aa62f0c5ee83c010
dfacoet <11651258+dfacoet@users.noreply.github.com>
2025-06-25 17:22:36 +0100

    err

--- scripts/deferred_literals.py
+++ scripts/deferred_literals.py
@@ -16,3 +16,11 @@ for lit in [1, 1.0, ibis.literal(1.0), ibis.deferred(1.0), ibis._(1.0)]:
         print(table.select(lit + ibis._["value"]).to_pandas())
     except Exception as e:
         print(f"Error: {e}")
+
+
+# Look into "Table is not callable" error
+
+
+expr = ibis._(1.0) + ibis._["value"]
+result = table.select(expr)
+print(result.to_pandas())
